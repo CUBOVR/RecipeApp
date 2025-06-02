3af32eb918715c43c65a97ef6e7cb38c9602dbec
CUBOVR <cristobalurquidesb@gmail.com>
2025-06-01 23:59:48 -0600

    Inicio proyecto con BottomNavBar

---
+++ lib/views/app_main_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class AppMainScreen extends StatefulWidget {
+  const AppMainScreen({super.key});
+
+  @override
+  State<AppMainScreen> createState() => _AppMainScreenState();
+}
+
+class _AppMainScreenState extends State<AppMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.green,
+        elevation: 0,
+        iconSize: 28,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.lime,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(
+          color: Colors.red,
+          fontWeight: FontWeight.w600,
+        ),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Iconsax.home5), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.heart5),
+            label: "Favorite",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.calendar5),
+            label: "Meal plan",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.setting_21),
+            label: "Setting",
+          ),
+        ],
+      ),
+    );
+  }
+}
