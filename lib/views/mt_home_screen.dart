dec224371056b6f5d4a604aa3e83a5156d076012
CUBOVR <cristobalurquidesb@gmail.com>
2025-06-03 23:47:50 -0600

    se añade componente de iconButton

--- lib/views/mt_home_screen.dart
+++ lib/views/mt_home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:recipe_app/widget/icon_button.dart';
 
 class MyHomeScreen extends StatefulWidget {
   const MyHomeScreen({super.key});
@@ -10,6 +12,40 @@ class MyHomeScreen extends StatefulWidget {
 class _MyHomeScreenState extends State<MyHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(backgroundColor: Colors.blueGrey);
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "What are you\ncooking today?",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            height: 1,
+                          ),
+                        ),
+                        MyIconButton(
+                          icon: Iconsax.notification,
+                          pressed: () {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
