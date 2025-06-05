ba49ffc65056456fb7468726a096fe45168d52ae
CUBOVR <cristobalurquidesb@gmail.com>
2025-06-04 23:16:10 -0600

    Se crea el elemento de busqueda, se crean metodos y se implementa el banner

--- lib/views/mt_home_screen.dart
+++ lib/views/mt_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:recipe_app/widget/banner.dart';
 import 'package:recipe_app/widget/icon_button.dart';
 
 class MyHomeScreen extends StatefulWidget {
@@ -22,23 +23,23 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 15),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(
-                      children: [
-                        Text(
-                          "What are you\ncooking today?",
-                          style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                            height: 1,
-                          ),
+                    headerParts(),
+                    searchBar(),
+                    //for banner
+                    const BannerToExplore(),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        "Categories",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
-                        MyIconButton(
-                          icon: Iconsax.notification,
-                          pressed: () {},
-                        ),
-                      ],
+                      ),
                     ),
+                    //for category
                   ],
                 ),
               ),
@@ -46,6 +47,47 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
           ),
         ),
       ),
+    );
+  }
+
+  Padding searchBar() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 22),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          prefixIcon: Icon(Iconsax.search_normal),
+          fillColor: Colors.white,
+          border: InputBorder.none,
+          hintText: "Search any recipes",
+          hintStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row headerParts() {
+    return Row(
+      children: [
+        Text(
+          "What are you\ncooking today?",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            height: 1,
+          ),
+        ),
+        const Spacer(),
+        MyIconButton(icon: Iconsax.notification, pressed: () {}),
+      ],
     );
   }
 }
