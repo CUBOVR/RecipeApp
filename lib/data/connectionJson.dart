0fcfdf9de007256de2caded10dfc4fa59d123f76
CUBOVR <cristobalurquidesb@gmail.com>
2025-06-10 23:44:30 -0600

    se agregar shared_preferences y se agrega en la conexion la actualizacion del json

--- lib/data/connectionJson.dart
+++ lib/data/connectionJson.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:recipe_app/data/models/infoRecipesModel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ConnectionJson {
   Future<InfoRecipesModel> loadAppInfo() async {
@@ -9,5 +10,32 @@ class ConnectionJson {
     );
     final Map<String, dynamic> jsonData = jsonDecode(rootFile);
     return InfoRecipesModel.fromJSON(jsonData);
+  }
+
+  Future<void> saveFavoriteStatus(String productId, bool isFavorite) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('favorite_$productId', isFavorite);
+  }
+
+  Future<bool> getFavoriteStatus(String productId) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('favorite_$productId') ?? false;
+  }
+
+  Future<Map<String, dynamic>> getCombinedData() async {
+    final data = await loadAppInfo();
+    final prefs = await SharedPreferences.getInstance();
+
+    final List<dynamic> recipes = data.recipes;
+
+    for (var item in recipes) {
+      final id = item['id'];
+      final isFavorite = prefs.getBool('favorite_$id');
+      if (isFavorite != null) {
+        item['isFavorite'] = isFavorite; // Sobrescribe el valor del JSON
+      }
+    }
+
+    return {'Recipes': recipes, 'App-Category': data.category};
   }
 }
