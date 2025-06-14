c2b1cda41e59a3e2b56fcf20c1713b842ed21a6a
CUBOVR <cristobalurquidesb@gmail.com>
2025-06-13 23:58:43 -0600

    Agregando información de la receta

--- lib/views/favorite_screen.dart
+++ lib/views/favorite_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe_app/Provider/favorite_provider.dart';
 import 'package:recipe_app/core/styles.dart';
 import 'package:recipe_app/data/connectionJson.dart';
+import 'package:recipe_app/data/models/infoRecipesModel.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -78,6 +80,34 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
                                     ),
                                   ),
                                 ),
+                                SizedBox(width: 10),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      favoriteItem.title,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Row(
+                                      children:
+                                          favoriteItem.terms
+                                              .map(
+                                                (term) =>
+                                                    construirWidgetsTermino(
+                                                      term,
+                                                    ),
+                                              )
+                                              .expand(
+                                                (widgetList) => widgetList,
+                                              )
+                                              .toList(),
+                                    ),
+                                  ],
+                                ),
                               ],
                             ),
                           ),
@@ -92,5 +122,49 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
         },
       ),
     );
+  }
+
+  List<Widget> construirWidgetsTermino(Terms term) {
+    switch (term.slug) {
+      case 'time':
+        return [
+          iconAditional(Iconsax.clock),
+          aditionalInfo(term.display),
+          aditionalInfo(" - "),
+        ];
+      case 'skillLevel':
+        return [
+          iconAditional(Iconsax.flash),
+          aditionalInfo(term.display),
+          aditionalInfo(" - "),
+        ];
+      case 'vegetarian':
+      case 'vegan':
+      case 'gluten-free':
+        return [iconAditional(Iconsax.heart), aditionalInfo(term.display)];
+      case 'healthy':
+        return [
+          iconAditional(Iconsax.health),
+          aditionalInfo(term.display),
+          aditionalInfo(" - "),
+        ];
+      default:
+        return []; // No mostrar nada si no lo reconoces
+    }
+  }
+
+  Text aditionalInfo(String value) {
+    return Text(
+      value,
+      style: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.bold,
+        color: Colors.blueGrey,
+      ),
+    );
+  }
+
+  Icon iconAditional(IconData icon) {
+    return Icon(icon, size: 16, color: Colors.blueGrey);
   }
 }
